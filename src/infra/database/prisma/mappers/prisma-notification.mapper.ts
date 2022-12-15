0d50c43da72d561040606d118122153e66962b4f
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-12-14 21:29:45 -0300

    feat: create toDomain method in prisma mapper

--- src/infra/database/prisma/mappers/prisma-notification.mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification.mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as PrismaNotification } from '@prisma/client';
 import { Notification } from '@core/entities/notification/notification.entity';
+import { Content } from '@core/entities/notification/content.entity';
 
 export class PrismaNotificationMapper {
   static toPersistence(notification: Notification) {
@@ -9,5 +11,19 @@ export class PrismaNotificationMapper {
       content: notification.content.value,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(prismaNotification: PrismaNotification) {
+    return new Notification(
+      {
+        recipientId: prismaNotification.recipientId,
+        category: prismaNotification.category,
+        content: new Content(prismaNotification.content),
+        readAt: prismaNotification.readAt,
+        canceledAt: prismaNotification.canceledAt,
+        createdAt: prismaNotification.createdAt,
+      },
+      prismaNotification.id,
+    );
   }
 }
